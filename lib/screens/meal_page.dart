e54cf6a48217039f1446364b2c640b6c895924ff
AiMouad <mouad14anes@gmail.com>
2025-05-24 17:14:13 +0100

    mealpage edit

--- lib/screens/meal_page.dart
+++ lib/screens/meal_page.dart
@@ -6,8 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
 class MealPage extends StatelessWidget {
-  MealPage({super.key, required this.meal});
   final MealModel meal;
+
+  MealPage({super.key, required this.meal});
 
   @override
   Widget build(BuildContext context) {
@@ -73,82 +74,101 @@ class MealPage extends StatelessWidget {
             ],
           ),
           Positioned(
-            top: 40.h - 50,
+            top: 40.h - 30,
             left: 0,
             right: 0,
             bottom: 0,
             child: SingleChildScrollView(
               child: Container(
                 decoration: BoxDecoration(
-                  color: AppTheme.bgColor,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(25.sp),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        meal.name ?? 'No Name',
-                        style: TextStyle(
-                          fontSize: 25.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 1.h),
-                      Row(
-                        children: List.generate(
-                          5,
-                          (index) => Icon(
-                            index < (meal.rating ?? 0)
-                                ? Icons.star
-                                : Icons.star_border,
-                            color:
-                                index < (meal.rating ?? 0)
-                                    ? Colors.amber
-                                    : AppTheme.black,
-                            size: 23.sp,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 2.h),
-                      Text(
-                        'Description :',
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 1.h),
-                      Text(
-                        meal.description ?? 'No Description',
-                        style: AppTheme.paragraphStyle,
-                      ),
-                      SizedBox(height: 3.h),
-                      Text(
-                        'Comment :',
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 1.h),
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: AppTheme.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: AppTheme.black, width: 1),
-                        ),
-                        padding: EdgeInsets.all(15.sp),
-                        child: Text(
-                          meal.comment ?? 'No Comment',
-                          style: TextStyle(fontSize: 16.sp),
-                        ),
-                      ),
-                      SizedBox(height: 2.h),
-                    ],
+                  color: AppTheme.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
                   ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10,
+                      offset: Offset(0, -3),
+                    ),
+                  ],
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 4.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      meal.name ?? 'No Name',
+                      style: TextStyle(
+                        fontSize: 22.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppTheme.primaryColor,
+                      ),
+                    ),
+                    SizedBox(height: 1.5.h),
+                    Row(
+                      children: List.generate(
+                        5,
+                        (index) => Icon(
+                          index < (meal.rating ?? 0)
+                              ? Icons.star
+                              : Icons.star_border,
+                          color:
+                              index < (meal.rating ?? 0)
+                                  ? Colors.amber
+                                  : AppTheme.black,
+                          size: 20.sp,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 3.h),
+                    Text(
+                      'Description:',
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: 1.h),
+                    Text(
+                      meal.description ?? 'No Description',
+                      style: AppTheme.paragraphStyle,
+                    ),
+                    SizedBox(height: 3.h),
+                    Text(
+                      'Comment:',
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: 1.h),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: AppTheme.white,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: AppTheme.primaryColor,
+                          width: 1,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(12.sp),
+                      child: Text(
+                        meal.comment ?? 'No Comment',
+                        style: TextStyle(fontSize: 13.sp),
+                      ),
+                    ),
+                    SizedBox(height: 30.h),
+                  ],
                 ),
               ),
             ),
